1f025bf8142d49f3507f83b627d8246809924b1f
Raraa <raraasukaicecreamm@gmail.com>
2025-09-08 12:11:04 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  BOT_TOKEN: "7100906830:AAF1HP5MmRXL2pUB9c8Eioch1pijNMe9_RE",
+};
+
+/*/━━━━━━━━━━━━━━━━━━━━━━━━  
+ SCARRY DEATH NEW VERSION
+━━━━━━━━━━━━━━━━━━━━━━━━━  
+Developer: Raraa
+Telegram: @raraa_imuppp
+Version: 3.0
+*/
